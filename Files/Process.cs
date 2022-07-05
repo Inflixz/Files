ac3e8c1ab51e1345bf9bb24481a5b2e82cb82986
nekitnigmatulin@gmail.com <nekitnigmatulin@gmail.com>
2022-07-05 16:26:30 +0300

    Сделал процесс

--- Files/Process.cs
+++ Files/Process.cs
@@ -6,9 +6,41 @@ using System.Threading.Tasks;
 
 namespace Files
 {
-    class Process
+    class Process///процес взаимодействия с пользователем
     {
-       
-
+        public Process()
+        {
+            interaction = new Interaction();
+        }
+        Interaction interaction;
+        public void Menu()
+        {
+            interaction.ShowTextWriteLine("1 - создать файл \n 2 - прочитать файл \n 3 - выход");
+        }
+        public void User()
+        {
+            WorkWithFiles workWithFiles = new WorkWithFiles();
+            int number;
+            string input = interaction.TakeText();
+            int.TryParse(input, out number);
+            while (number != 3)
+            {
+                interaction.ShowTextWriteLine("Напшите путь вашего файла");
+                string pathgOfFile = interaction.TakeText();
+                switch (number)
+                {
+                    case 1:
+                        {
+                            workWithFiles.CreateFile(pathgOfFile);
+                            break;
+                        }
+                    case 2:
+                        {
+                            workWithFiles.ReadFile(pathgOfFile);
+                            break;
+                        }
+                }
+            }
+        }
     }
 }
